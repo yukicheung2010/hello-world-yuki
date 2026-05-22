ebe61aecfa3ab31a7ef9212125efd49aed34ad84
yukicheung2010 <yuki.cheung@stu.ocsb.ca>
2026-05-21 21:59:56 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,4 @@
+def on_forever():
+    basic.show_string("Hello Yuki")
+    basic.show_icon(IconNames.HEART)
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,4 @@
+basic.forever(function () {
+    basic.showString("Hello Yuki")
+    basic.showIcon(IconNames.Heart)
+})
